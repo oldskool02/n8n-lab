b477cb9abf1ef9c5eb31a407d5f5feb30aab14f6
Ian Wegerle <ian@oldskool.co.za>
2026-06-26 12:27:11 +0000

    Remove abandoned CRM project

--- crm/backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .account import Account
-from .user import User
-from .product import Product
-from .contact import Contact
-from .document import Document
-from .document_item import DocumentItem
-from .interaction import Interaction
