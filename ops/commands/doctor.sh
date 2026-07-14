90d8453a827b37dfb8bac547128e028b7874bd73
Ian Wegerle <ian@oldskool.co.za>
2026-07-14 12:20:42 +0000

    Add initial Docker subsystem checks to doctor command

--- ops/commands/doctor.sh
+++ ops/commands/doctor.sh
@@ -6,8 +6,8 @@ OPS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 source "$OPS_ROOT/lib/config.sh"
 
-OK="✔️"
-FAIL="❌"
+source "$OPS_ROOT/lib/common.sh"
+
 
 check_program()
 {
@@ -18,14 +18,15 @@ check_program()
 
     local DISPLAY_NAME="$1"
     local COMMAND_NAME="$2"
-    VERSION="$("$COMMAND_NAME" --version)"
+    # VERSION="$("$COMMAND_NAME" --version)"
 
     echo "Checking ${DISPLAY_NAME}..."
 
     if command -v "$COMMAND_NAME" >/dev/null 2>&1
     then
+        local VERSION="$("$COMMAND_NAME" --version)"
         echo "${OK} ${DISPLAY_NAME} is installed"
-        echo "   Version : ${VERSION}"
+        echo "  Version : ${VERSION}"
     else
         echo "${FAIL} ${DISPLAY_NAME} is NOT installed"
     fi
@@ -33,12 +34,43 @@ check_program()
     echo
 }
 
+check_docker()
+{
+    echo "----------------"
+    echo "Docker Subsystem"
+    echo "----------------"
+
+    # echo "Check Docker CLI"
+    check_program "Docker CLI" docker
+
+    echo
+    echo "Check Docker Compose"
+    if docker compose version >/dev/null 2>&1
+    then
+      local VERSION="$(docker compose version)"
+      echo "${OK} Docker Compose is available"
+      echo "  Version: ${VERSION}"
+    fi
+
+    echo
+    echo "Check Docker Daemon"
+    if docker ps >/dev/null 2>&1
+    then
+      local VERSION="$(docker ps)"
+      echo "${OK} Docker Daemon is available"
+    #   echo "  Version: ${VERSION}"
+    fi
+
+    # Check Docker Daemon
+}
 
 echo "=================================="
 echo " Operations Toolkit - Doctor"
 echo "=================================="
 echo
 
-check_program "Docker" docker
+# check_program "Docker" docker
 check_program "Git" git
 check_program "Python" python3
+
+check_docker
